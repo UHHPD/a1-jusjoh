5094c2fabe19a6930236a5c1f98b4bcf7245e93a
jwilckens <102976232+jwilckens@users.noreply.github.com>
2022-05-02 20:20:14 +0000

    Poissonverteilung Aufgabe 1c Anfang

--- poisson.cc
+++ poisson.cc
@@ -4,22 +4,36 @@
 #include <cmath>
 using namespace std;
 
-//File Declaration
-ofstream fout ("hist.txt");
 
-int main(){
+
+double poisson(double mu, int k){//Berechnet die Poissonwahrscheinlichkeit (mu^k)*exp(-mu)/k!
+  muk = pow(mu,k);
+  expmu = exp(-mu);
+  k_fak = tgamma(k);
+  return muk*expmu/k_fak;
+}
+
+int Poissonverteilung_1ab(){
   int zahl;
+  ofstream fout ("hist.txt");
   ifstream fin("datensumme.txt");
   vector<int> zaehler(11);
 
-  for(int i = 0; i < 234; i++){
+  for(int i = 0; i < 234; i++){//Liest aus Datensumme aus und zählt die jeweiligen Anzahlen der Ziffern 0-9
     fin >> zahl;
     zaehler[zahl] += 1;
   }
-  for(int i = 0; i < zaehler.size(); i++){
+  for(int i = 0; i < zaehler.size(); i++){//Schriebt Inhalt des Vektors in hist.txt 
     //cout << zaehler[i] << endl;
     fout << i << " " << zaehler[i] << endl;
   }
+  
+}
+int main(){
+  Poissonverteilung_1ab();
+  double mu = 3,11538;
+  
+  
 }
 
 
