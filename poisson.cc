ee72984502873e6bfa07a3cc166d45e0de326fe0
jwilckens <102976232+jwilckens@users.noreply.github.com>
2022-05-02 19:45:23 +0000

    Code zum Erstellen der Hist.txt

--- poisson.cc
+++ poisson.cc
@@ -4,15 +4,21 @@
 #include <cmath>
 using namespace std;
 
+//File Declaration
+ofstream fout ("hist.txt");
+
 int main(){
   int zahl;
   ifstream fin("datensumme.txt");
   vector<int> zaehler(11);
 
-  for(i = 0; i < 234; i++){
+  for(int i = 0; i < 234; i++){
     fin >> zahl;
     zaehler[zahl] += 1;
-    
+  }
+  for(int i = 0; i < zaehler.size(); i++){
+    //cout << zaehler[i] << endl;
+    fout << i << " " << zaehler[i] << endl;
   }
 }
 
