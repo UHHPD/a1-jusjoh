0bc907136388487c07cc859f6fb3cb3ab777477e
jwilckens <102976232+jwilckens@users.noreply.github.com>
2022-05-02 20:45:15 +0000

    Kleine Kosmetische Anpassung

--- Aufgabenblatt_1_2/stichprobe2.cc
+++ Aufgabenblatt_1_2/stichprobe2.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-using namespace std; //Setzt den Namespace fest, dann muss man das nicht immer std::cout oder so schreiben. Es reicht cout
+using namespace std;
 
 double mittelwert_berechnen(double x, int N){//Berechnet Mittelwert gegebener Summe und Anzahl
   return x/N;
@@ -19,12 +19,12 @@ double mw_mw(ifstream & fin_mw){//Berechnet die Mittelwerte aus Dateien: mittelw
 
 int main(){
   double summe, zahl, mw, varianz, st_abweich;
-  ifstream fin    ("datensumme.txt"); //öffnet die Input-Datei
-  ifstream fin2   ("datensumme.txt"); //öffnet die Input-Datei
-  ofstream fout   ("mittelwerte.txt");
-  ofstream fout2  ("varianzen.txt");
+  ifstream fin    ("datensumme.txt"); //öffnet eine Input-Datei
+  ifstream fin2   ("datensumme.txt"); //öffnet eine Input-Datei
+  ofstream fout   ("mittelwerte.txt");//öffnet eine Output-Datei
+  ofstream fout2  ("varianzen.txt");  //öffnet eine Output-Datei
   
-  for( int j = 0; j < 26; j++){ // Läuft durch jede Zeile und Schreibt die Summe in die Output-Datei
+  for( int j = 0; j < 26; j++){//Berechnet den Mittelwert über alle 9er Zahlengruppen in Datensummen
     summe = 0;
     for( int i = 0; i < 9; i++){
       fin >> zahl;
@@ -32,7 +32,7 @@ int main(){
     }
     mw = mittelwert_berechnen(summe, 9);
     summe = 0;
-    for( int i = 0; i < 9; i++){
+    for( int i = 0; i < 9; i++){//Berechnet die Varianz über alle 9er Zahlengruppen in Datensummen
     fin2 >> zahl;
     summe += (zahl-mw)*(zahl-mw);
   }
@@ -42,15 +42,17 @@ int main(){
     fout2 << varianz << endl;
   } 
 
-  
-
-  fin.close();  // Schließt die Input-Datei
-  fout.close(); 
-  fout2.close(); 
+  fin.close();
+  fin2.close();
+  fout.close();
+  fout2.close();
   
   ifstream fin_mw ("mittelwerte.txt");
   ifstream fin_v  ("varianzen.txt");
   cout << "Mittelwert der Mittelwerte: " << mw_mw(fin_mw) << endl;
   cout << "Mittelwert der Varianzen: " << mw_mw(fin_v) << endl;
 }
-
+/*
+Mittelwert der Mittelwerte: 3.11539
+Mittelwert der Varianzen: 2.4359
+*/
